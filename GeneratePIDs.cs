691c0085dc7e18d30b63be28e46e4b6a20f4b32b
Rich King <rking@rebgv.org>
2020-05-08 15:50:20 -0700

    Building the generation logic

--- GeneratePIDs.cs
+++ GeneratePIDs.cs
@@ -4,9 +4,25 @@ namespace REBGV.Functions
 {
     public class GeneratePIDs
     {
-        public static List<string> Generate()
+        private static int _limit { get; } = 10;
+        private static List<string> _pids { get; } = new List<string>();
+
+        public static void Generate(int quantity)
         {
+            // Fetches the latest PID from blob storage.
             
+            int latestPid = int.Parse("111111111");
+
+            // Validates the quantity, to make sure the built-in limit isn't exceeded.
+
+            quantity = quantity <= _limit ? quantity : _limit;
+
+            // Creates PIDs equal to the quantity requested.
+
+            for(int i = 0; i < quantity; i++)
+            {
+                _pids.Add(new Pid(++latestPid));
+            }
         }
     }
 }
