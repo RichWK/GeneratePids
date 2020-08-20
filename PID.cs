2f27d669afca9ed1963841e28edb68bbaa63399d
Rich King <rking@rebgv.org>
2020-08-20 16:32:00 -0700

    Edited comment for clarity

--- PID.cs
+++ PID.cs
@@ -8,7 +8,7 @@ namespace REBGV.Functions
             get { return RawPid.ToString().Insert(3, "-").Insert(7, "-"); }
         }
 
-        // The constructor sets the PID number based on the parameter passed to it.
+        // The constructor sets the PID's number based on the integer passed to it.
 
         public PID(int currentPid)
         {
