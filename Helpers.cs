33dcb37fc760d3959c863fb738efeffab92e03d9
Rich King <rking@rebgv.org>
2020-10-19 16:11:47 -0700

    Tidying up code

--- Helpers.cs
+++ Helpers.cs
@@ -9,16 +9,13 @@ namespace REBGV.Functions
         public static List<string> Generate(string startingPid, int quantity)
         {
             int pid;
+            List<string> pids = new List<string>();
 
             int.TryParse(startingPid, out pid);
 
             // Validate the quantity, to make sure the built-in limit isn't exceeded.
 
             quantity = quantity <= _limit ? quantity : _limit;
-
-            // Create PIDs equal to the quantity requested.
-
-            List<string> pids = new List<string>();
 
             for(int i = 0; i < quantity; i++)
             {
