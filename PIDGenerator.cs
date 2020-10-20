c4af6faa8cc0c6d876774bf3d69f0b4529a5acd0
Rich King <rking@rebgv.org>
2020-10-20 13:53:03 -0700

    Added method to build configuration

--- PIDGenerator.cs
+++ PIDGenerator.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using static REBGV.Functions.Helpers;
 using System.IO;
 using System.Threading.Tasks;
 
+
+
 namespace REBGV.Functions
 {
+
     public static class PIDGenerator
     {
+
+        public static IConfiguration Config { get; private set; }
+
         /* This method, Run(), is the entry point.
 
         It extracts a quantity from an incoming POST request, so long as a quantity was
@@ -37,10 +45,13 @@ namespace REBGV.Functions
                 PartitionKey = "1"
             )]
             PidDbItem pidDbItem,
+            ExecutionContext context,
             ILogger log
         )
         {
             log.LogInformation("PIDGenerator function began processing a request.");
+
+            FetchConfigurationDetails(context);
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
@@ -58,5 +69,29 @@ namespace REBGV.Functions
 
             return new OkObjectResult(responseMessage);
         }
+
+
+
+        public static void FetchConfigurationDetails(ExecutionContext context)
+        {
+            /* This pulls in settings regardless of whether the function is running in a
+            local development environment or in production on Azure. */
+
+            Config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables() 
+                .Build(); 
+        }
+
+
+
+        // public static async void UpdateDatabase()
+        // {
+            
+
+        //     using CosmosClient client = new CosmosClient();
+        // }
     }
 }
+
